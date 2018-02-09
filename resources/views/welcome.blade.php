8550d1d4cdec2746466e46df8065c3212670e7a2
juanfll023 <juanfll023@gmail.com>
2018-02-09 15:17:09 -0400

    anexando dropdowns

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,6 +15,18 @@
     </head>
     <body>
 
+<ul class="nav nav-tabs">
+  ...
+  <li role="presentation" class="dropdown">
+    <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+      Dropdown <span class="caret"></span>
+    </a>
+    <ul class="dropdown-menu">
+      ...
+    </ul>
+  </li>
+  ...
+</ul>
 
 <!-- Single button -->
 <div class="btn-group">
