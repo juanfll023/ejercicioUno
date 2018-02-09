151217b5a7421ab3fc70f6002824fcc5921e8d14
juanfll023 <juanfll023@gmail.com>
2018-02-09 14:09:00 -0400

    Se agregaron nuevos componentes HTML

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,6 +22,19 @@
   <strong>Heads up!</strong> This alert needs your attention, but it's not super important.
 </div>
 
+<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="..." alt="...">
+      <div class="caption">
+        <h3>Thumbnail label</h3>
+        <p>...</p>
+        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+      </div>
+    </div>
+  </div>
+</div>
+
         <div id="main" class="container">
             <div class="row">
                 <div class="col-sm-4">
