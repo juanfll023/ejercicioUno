a5ac3945ef621d9162e0b311ba55e6fd9f478829
juanfll023 <juanfll023@gmail.com>
2018-02-09 11:45:46 -0400

    Subiendo Archivos

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel & Vue.js</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+
+    </head>
+    <body>
+
+        <div id="main" class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                    <h1>VUEjs - AJAX axios</h1>
+                    <ul class="list-group">
+                        <li v-for="item in lists" class="list-group-item">                      
+                            @{{ item.name }}
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-sm-8">
+                    <h1>JSON</h1>
+                    <pre>
+                        @{{ $data  }}
+                    </pre>
+                </div>
+            </div>
+        </div>
+
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
